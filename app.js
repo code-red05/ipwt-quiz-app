6946dbc67c5a61e5dd877323388e75e2bd2d151e
Mehul Rana <74825953+code-red05@users.noreply.github.com>
2021-05-28 20:01:03 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const app = express()
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', (req, res) => {
-        res.sendFile(__dirname + '/home.html');
+    res.sendFile(__dirname + '/home.html');
+})
+
+app.get('/login',(req,res) => {
+    res.sendFile(__dirname + '/login.html');
+})
+
+app.post('/login', (req,res) => {
+    
 })
 
 app.get('/attempt_quiz', (req, res) => {
