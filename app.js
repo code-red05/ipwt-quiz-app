3723748051dfe9d66eff08e3f48c7856ac296ff0
aneesh <aneeshs7bd@gmail.com>
2021-05-30 16:21:32 +0530

    Small change

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 app.use(express.static(__dirname + "/public"));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/home.html');
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/home.html");
+});
 
-app.get('/login',(req,res) => {
-    res.sendFile(__dirname + '/login.html');
-})
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/login.html");
+});
 
-app.post('/login', (req,res) => {
-    
-})
+app.post("/login", (req, res) => {});
 
-app.get('/attempt_quiz', (req, res) => {
-    res.sendFile(__dirname + '/attempt_quiz.html')
-})
-  
+app.get("/attempt_quiz", (req, res) => {
+  res.sendFile(__dirname + "/attempt_quiz.html");
+});
+
+app.get("/create_quiz", (req, res) => {
+  res.sendFile(__dirname + "/create_quiz.html");
+});
+app.post("/create_quiz", (req, res) => {
+  req.sendFile(__dirname + "/create_quiz.html");
+});
+
 app.listen(3000, () => {
-    console.log('App listening at http://localhost:3000')
-})
+  console.log("App listening at http://localhost:3000");
+});
